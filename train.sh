80e7d06d9a0d108c292c29ab3bee862346042292
Axel Giottonini <giottonini.axel@gmail.com>
2023-07-17 15:45:38 +0200

    code cleaning

--- train.sh
+++ train.sh
@@ -2,12 +2,12 @@
 
 CUDA_VISIBLE_DEVICES=0 python ./train.py \
     --model_name test \
-    --model_version 0.0 \
-    --learning_rate 0.0001 \
+    --model_version 1 \
+    --learning_rate 0.0004 \
     --training_set "./data/_test.csv" \
     --validation_set "./data/_val.csv" \
-    --max_length 400 \
-    --n_epochs 5 \
-    --batch_size 256 \
+    --max_length 512 \
+    --n_epochs 1 \
+    --global_batch_size 32 \
     --local_batch_size 16 \
-    --num_workers 16 
+    --num_workers 16
